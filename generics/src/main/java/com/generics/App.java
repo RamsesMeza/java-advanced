19ffb552ae3b6a7946e7b2a9c9c387b2026982b8
Ramses Meza <ramseswardof@gmail.com>
2026-04-06 01:07:25 -0600

    first commit, objects and generics types in java

---
+++ generics/src/main/java/com/generics/App.java
@@ -0,0 +1,9 @@
+package com.generics;
+
+public class App {
+    public static void main(String[] args) {
+
+        Person person = new Person(1, "Ramses", 23);
+        System.out.println("Hello World!" + person);
+    }
+}
---
+++ generics/src/main/java/com/generics/Person.java
@@ -0,0 +1,13 @@
+package com.generics;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Person {
+  private int id;
+  private String name;
+  private int age;
+
+}
