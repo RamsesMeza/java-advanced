da35a43dc97980e0c9d6f7d9288e745d89c26f7c
Ramses Meza <ramseswardof@gmail.com>
2026-05-08 07:54:22 -0600

    Delete generic exercises

--- generics/src/main/java/com/generics/App.java
+++
@@ -1,9 +0,0 @@
-package com.generics;
-
-public class App {
-    public static void main(String[] args) {
-
-        Person person = new Person(1, "Ramses", 23);
-        System.out.println("Hello World!" + person);
-    }
-}
