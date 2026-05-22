866c6e7d252aa1a58b587d0d67bbbc0e3ba7745f
Ramses Meza <ramseswardof@gmail.com>
2026-05-22 01:31:11 -0600

    Model layer, ProductRepository percistance

---
+++ optional-class/src/main/java/com/optional/finalproject/repository/ProductRepository.java
@@ -0,0 +1,74 @@
+package com.optional.finalproject.repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import com.optional.finalproject.model.Product;
+import com.optional.finalproject.model.ProductCategory;
+
+public class ProductRepository {
+  private final List<Product> products;
+
+  public ProductRepository(List<Product> products) {
+    this.products = products;
+  }
+
+  public void add(Product product) {
+    this.products.add(product);
+
+  }
+
+  public void remove(Product product) {
+    this.products.remove(product);
+  }
+
+  // Tinking if pass the Product complealty is correct or only with id
+  public void update(Product product) {
+
+    Optional<Product> oldProduct = this.products.stream().filter(p -> product.getId().equals(p.getId())).findFirst();
+
+    // Dont know if this validation lives here, becase could be and database error
+
+    int indexToDelete = this.products
+        .indexOf(oldProduct.orElseThrow(() -> new NoSuchElementException("DB: Product not found")));
+    this.products.set(indexToDelete, product);
+  }
+
+  public Optional<Product> findByName(String name) {
+
+    return this.products.stream()
+        .filter(p -> name.equals(p.getName()))
+        .findFirst();
+  }
+
+  public Optional<Product> findById(Long id) {
+    return this.products.stream()
+        .filter(p -> id.equals(p.getId()))
+        .findFirst();
+  }
+
+  public List<Product> findByCategory(ProductCategory category) {
+
+    List<Product> filteredList = this.products.stream()
+        .filter(p -> category.equals(p.getCategory()))
+        .toList();
+
+    return new ArrayList<>(filteredList);
+
+  }
+
+  public List<Product> findByPrice(double price) {
+    List<Product> filteredList = this.products.stream()
+        .filter(p -> price > p.getPrice())
+        .toList();
+
+    return new ArrayList<>(filteredList);
+  }
+
+  // public double averageByCategory() {
+
+  // }
+
+}
